da4c2f0cf3852ae3c20107d9bbfa10f7cbf06c2f
Sara Alharbi <sbalharbi22@twq.idserve.net>
2023-01-05 09:10:58 +0300

    Initial Commit

---
+++ Stay Focused2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Stay Focused2
+//
+//  Created by Sara bayan alharbi on 12/06/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
